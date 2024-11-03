509fb5a74e680515b26ff75572d974e400007c38
priyadigyna <digyna.priya@gmail.com>
2024-11-03 20:31:58 +0530

    first commit

---
+++ src/main/resources/data1.sql
@@ -0,0 +1,6 @@
+INSERT INTO  product (name, desc, brand, price, category, release_Date, available, quantity) VALUES
+('Tata Nexon', 'A compact SUV with excellent safety features and performance.', 'TATA MOTORS', 789999, 'Cars', '2024-01-15', true, 50),
+('Maruthi Suzuki', 'A compact SUV with good efficiency and reliability.', 'Maruthi Suzuki', 765999, 'Cars', '2024-02-15', true, 60),
+('Hyundai Creta', 'A Stylish SUV with advanced features and comfortable interior.', 'Hyundai', 980000, 'Cars', '2024-03-15', true, 80),
+('Mahindra Thar', 'A rugged off-road SUV with a powerful engine and modern amenities.', 'Mahindra', 1200000, 'Cars', '2024-04-15', true, 90),
+('Honda City', 'A premium sedan with a sleek design and advanced safety features.','Honda', 1100000, 'Cars', '2024-05-15', true, 30);
